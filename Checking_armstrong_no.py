681c2a92c1c53fc5f4b57557f34a03020a6ac8ce
Levishanungchim <109846961+Levishanungchim@users.noreply.github.com>
2022-07-24 11:29:22 +0530

    Add files via upload

---
+++ Checking_armstrong_no.py
@@ -0,0 +1,11 @@
+n=eval(input("Enter a number : "))
+t=n
+sum=0
+while n!=0:
+    d=n%10
+    sum+=(d**3)
+    n//=10
+if t==sum:
+   print(f"Given number {t} is Amstrong ")
+else:
+   print(f"Given number {t} is not an Amstrong")
---
+++ Factorial.py
@@ -0,0 +1,6 @@
+n=eval(input("Enter a value for n : "))
+i,fact=1,1
+while i<=n:
+ fact*=i
+ i+=1
+print(f"Factorial of  {n} = {fact}")
